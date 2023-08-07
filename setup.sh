12167f2fe83572d799322e42fb18fb443625f389
Stretgaming <119709303+Stretgaming@users.noreply.github.com>
2023-08-07 13:15:38 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,16 @@
+npm i requests
+npm i playwright-extra
+npm i colors
+npm i https-proxy-agent
+npm i crypto-random-string
+npm i events
+npm i fs
+npm i net
+npm i cloudscraper
+npm i request
+npm i hcaptcha-solver
+npm i randomstring
+npm i cluster
+npm i cloudflare-bypasser
+npm i http http2 crypto tls
+pip3 install -r requirements.txt
